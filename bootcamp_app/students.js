a866a3ddaa178eabfc71441fdbcf70d6318d67da
Mai Duo Wu <wmaiduo@gmail.com>
2021-09-16 06:16:36 +0000

    added parameterized queries for SQL

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,20 +1,28 @@
-const { Pool } = require('pg');
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
-});
-
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    console.log(res.rows);
+  })
+  .catch((err) => console.error("query error", err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,15 @@
 const { Pool } = require('pg');
-console.log(process.argv[2]);
+
+const cohortName = [process.argv[2] || 'JUL02'];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,15 +18,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+pool.query(queryString, cohortName)
 .then(res => {
   console.log(res.rows);
 })
